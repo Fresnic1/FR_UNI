d5801d76ea41d157f4427b186f0c9c6d659cb2aa
Fresnic <73668911+Fresnic1@users.noreply.github.com>
2025-01-25 13:23:41 +0200

    Added NetScan to UNI

--- UNI.py
+++ UNI.py
@@ -8,14 +8,20 @@ print(Fore.BLUE + text)
 def run_keylogger():
     os.system('python scripts/FR_KeyLogger/KeyLog.py')
 
+def run_netscan():
+    os.system('python scripts/FR_NetScan/NetScan.py')
+
 def main():
     print("Select the script you want to run:")
     print("1. KeyLogger (Alpha)")
+    print("2. NetScan")
 
     choice = input("Enter the number of your choice: ")
 
     if choice == '1':
         run_keylogger()
+    elif choice == '2':
+        run_netscan()
     else:
         print('\033[31mError code: 001\033[0m {Choose a valid option}')
 
